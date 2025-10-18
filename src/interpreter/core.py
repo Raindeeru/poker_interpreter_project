3704921899b50c1d07014f88f2ffcb5958a51277
Raindeeru <derv.gonzales17@gmail.com>
2025-10-18 19:47:05 +0800

    core and tokenizer

---
+++ src/interpreter/core.py
@@ -0,0 +1,25 @@
+from poker_game.state import State
+from interpreter.parser import parser
+from interpreter.tokenizer import lexer
+import interpreter.semantic_analyzer as s
+import interpreter.tokenizer as t
+
+
+def interpret_command(input: str, state: State):
+    input = input.lower()
+
+    lexer.input(input)
+
+    parsed = parser.parse(input)
+
+    if t.error:
+        return t.error
+
+    if not parsed:
+        return "Syntax Error"
+
+    valid = s.valid_semantics(parsed)
+
+    if not valid[0]:
+        return valid[1]
+
