53b010851b985e15a62df04413bceaecd49f309e
Raindeeru <derv.gonzales17@gmail.com>
2025-10-18 16:44:32 +0800

    semantic analyzer

---
+++ src/interpreter/semantic_analyzer.py
@@ -0,0 +1,21 @@
+import interpreter.parser as p
+
+
+def check_bet_valid(target):
+    if target is p.Number():
+        return True
+    else:
+        return (False, "Wrong Target on the Bet Command!")
+
+
+def check_quit_valid(target):
+    if target is None:
+        return True
+    else:
+        return (False, "Quit has targets!")
+
+
+def valid_semantics(ast):
+    if ast.command == "bet":
+        return check_bet_valid(ast.target)
+    pass
