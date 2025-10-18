92b924199157a4253b96737e1f329144aa06df2a
balongie <butiubalong@gmail.com>
2025-10-18 17:06:06 +0800

    test semantic

--- src/interpreter/semantic_analyzer.py
+++ src/interpreter/semantic_analyzer.py
@@ -2,7 +2,7 @@ import interpreter.parser as p
 
 
 def check_bet_valid(target):
-    if target is p.Number():
+    if target is p.Number:
         return True
     else:
         return (False, "Wrong Target on the Bet Command!")
@@ -14,8 +14,20 @@ def check_quit_valid(target):
     else:
         return (False, "Quit has targets!")
 
+def check_use_valid(target):
+    if isinstance(target, p.SpecialCardCommand):
+        return True
+    else:
+        return (False, "Use command must target valid card")
+
 
 def valid_semantics(ast):
     if ast.command == "bet":
         return check_bet_valid(ast.target)
+    
+    if ast.command == "use":
+        return check_use_valid(ast.target)
+    
+
     pass
+
