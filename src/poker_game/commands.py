139807b2fd7d7d1f2e30988e89c1f9403c513b95
balongie <butiubalong@gmail.com>
2025-10-19 23:19:15 +0800

    format in commands.py

--- src/poker_game/commands.py
+++ src/poker_game/commands.py
@@ -4,3 +4,30 @@ from poker_game.state import State
 def Start(state: State):
     state.started = True
     return state
+
+def Bet(state: State):
+    pass
+
+def Fold(state: State):
+    pass
+
+def Call(state: State):
+    pass
+
+def All(state: State):
+    pass
+
+def Raise(state: State):
+    pass
+
+def Buy(state: State):
+    pass
+
+def Inspect(state: State):
+    pass
+
+def Play(state: State):
+    pass
+
+def Quit(state: State):
+    pass
