bfeb02d47df2371a65a9d566de7e968736bd1ccc
balongie <butiubalong@gmail.com>
2025-10-20 00:00:25 +0800

    added basic commands

--- src/poker_game/commands.py
+++ src/poker_game/commands.py
@@ -5,22 +5,33 @@ def Start(state: State):
     state.started = True
     return state
 
-def Bet(state: State):
-    pass
+def Bet(state: State, bet:int):
+    state.player_chips -= bet
+    state.pot += bet
+    state.player_last_bet = bet
+    return state
 
 def Fold(state: State):
     pass
 
 def Call(state: State):
-    pass
+    state.pot += state.enemy_last_bet
+    state.player_chips -= state.enemy_last_bet
+    return state
 
 def All(state: State):
-    pass
+    state.pot += state.player_chips
+    state.player_chips = 0
+    return state
 
-def Raise(state: State):
-    pass
+def Raise(state: State, raise_val:int):
+    state.pot += state.enemy_last_bet + raise_val
+    state.player_chips -= state.enemy_last_bet + raise_val
+    return state
 
-def Buy(state: State):
+def Buy(state: State, shop_index:int):
+    # state.player_chips -= state.shop_items[shop_index].price
+    # state.player_deck.append(state.shop_items[shop_index].card)
     pass
 
 def Inspect(state: State):
--- src/poker_game/state.py
+++ src/poker_game/state.py
@@ -10,15 +10,24 @@ class State:
     in_game: bool = False
 
     # Siguro dito yung laman ng list is dict ng {cardid:CardId, cardType: str}
-    player_cards = []
-    enemy_cards = []  # Dito siguro mas isa pa na laman yung dict kung
+    player_deck = []
+    player_hand = []
+    enemy_hand = []
+    enemy_deck = []  # Dito siguro mas isa pa na laman yung dict kung
     community_cards = [] # revealed siya or hindi
 
     player_health: int = 0
     enemy_health: int = 0
 
+    #Betting
+    enemy_last_bet: int = 0
+    player_last_bet: int = 0
+
+    #Pera
     player_chips: int = 0
     enemy_chips: int = 0
 
+    shop_items = []
+    pot: int = 0
     # di ko alam rn pano to, pero probably gagamitin for the ai
     # enemy_state: str
