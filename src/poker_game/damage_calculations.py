2eb1affead1f4a5132acb5ccca9cb388804fe3e1
Barusu <payormatthewjoshguerrero@gmail.com>
2025-10-23 00:38:19 +0800

    Damage Calculation except for 2 pair

--- src/poker_game/damage_calculations.py
+++ src/poker_game/damage_calculations.py
@@ -15,6 +15,34 @@ def calculate_Straight_Flush(state: State, cards_held):
     print(state.player_damage)
     return (state)
 
+def calculate_Four_of_a_Kind(state: State, cards_held):
+    count = {}
+    print(len(cards_held))
+    for card in cards_held:  
+        value = card.value   
+        count[value] = count.get(value, 0) + 1
+    for value in count:
+        if count[value] == 4:
+            state.player_damage += value * 4
+            state.player_damage = state.player_damage * 8
+            print(state.player_damage)
+            break
+    return (state)
+
+def calculate_Full_House(state: State, cards_held):
+    for card in cards_held:
+        state.player_damage += card.value
+    state.player_damage = state.player_damage * 7
+    print(state.player_damage)
+    return (state)
+
+def calculate_Flush(state: State, cards_held):
+    for card in cards_held:
+        state.player_damage += card.value
+    state.player_damage = state.player_damage * 6
+    print(state.player_damage)
+    return (state)
+
 def calculate_Straight(state: State, cards_held):
     for card in cards_held:
         if cards_held[0].value == 2 and cards_held[4].value == 14:
@@ -26,6 +54,42 @@ def calculate_Straight(state: State, cards_held):
         state.player_damage += card.value
 
     state.player_damage = state.player_damage * 5
+    print(state.player_damage)
+    return (state)
+
+def calculate_Three_of_a_kind(state: State, cards_held):
+    count = {}
+    print(len(cards_held))
+    for card in cards_held:  
+        value = card.value   
+        count[value] = count.get(value, 0) + 1
+    for value in count:
+        if count[value] == 3:
+            state.player_damage += value * 3
+            state.player_damage = state.player_damage * 4
+            print(state.player_damage)
+            break
+    return (state)
+
+def calculate_Two_Pair(state: State, cards_held):
+    pass
+
+def calculate_Pair(state: State, cards_held):
+    count = {}
+    print(len(cards_held))
+    for card in cards_held:  
+        value = card.value   
+        count[value] = count.get(value, 0) + 1
+    for value in count:
+        if count[value] == 2:
+            state.player_damage += value * 2
+            state.player_damage = state.player_damage * 2
+            print(state.player_damage)
+            break
+    return (state)
+
+def calculate_High_Card(state: State, cards_held):
+    state.player_damage = cards_held[4].value
     print(state.player_damage)
     return (state)
 
@@ -42,27 +106,28 @@ def damage_calculation(state: State):
 
         return calculate_Straight_Flush(state, cards_held)
 
-    elif pattern_Found == Four_of_a_Kind:
-        pass 
+    elif pattern_Found == "Four_of_a_Kind":
+        return calculate_Four_of_a_Kind(state, cards_held)
 
-    elif pattern_Found == Full_House:
-        pass 
+    elif pattern_Found == "Full_House":
+        return calculate_Full_House(state, cards_held)
 
-    elif pattern_Found == Flush:
-        pass 
+    elif pattern_Found == "Flush":
+        return calculate_Flush(state, cards_held) 
 
     elif pattern_Found == "Straight":
         return calculate_Straight(state, cards_held)
 
-    elif pattern_Found == Three_of_a_Kind:
-        pass 
-    elif pattern_Found == Two_Pair:
-        pass 
+    elif pattern_Found == "Three_of_a_Kind":
+        return calculate_Three_of_a_kind(state, cards_held)
+    
+    elif pattern_Found == "Two_Pair":
+        return calculate_Two_Pair(state, cards_held) 
 
-    elif pattern_Found == Pair:
-        pass 
+    elif pattern_Found == "Pair":
+        return calculate_Pair(state, cards_held)
     else:
-        pass
+        return calculate_High_Card(state, cards_held)
     
 
     
