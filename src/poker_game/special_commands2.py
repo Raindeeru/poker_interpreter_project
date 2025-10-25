fa76c7bb99820a0086d93a0935f3b7a3891ae4fc
balongie <butiubalong@gmail.com>
2025-10-25 15:03:18 +0800

    exchange command

--- src/poker_game/special_commands2.py
+++ src/poker_game/special_commands2.py
@@ -38,6 +38,13 @@ def check_if_special_card_exists(state: State, card: Card):
     )
     return exists
 
+def check_if_card_in_hand(state: State, card: Card):
+    exists = any(
+        c.suit == card.suit and
+        c.value == card.value 
+        for c in state.player_hand
+    )
+    return exists
 
 def Reveal(state: State, index: int, card: Card):
     if not check_if_special_card_exists(state, card):
@@ -51,8 +58,22 @@ def Reveal(state: State, index: int, card: Card):
             f"Revealed {get_card_string(state.enemy_hand[index])}")
 
 
-def Exchange(state: State):
-    pass
+def Exchange(state: State, index: int, card: Card, special_card: Card):
+
+    if not check_if_special_card_exists(state, special_card):
+        return (state, False, "You don't have that special card")  
+    elif not check_if_card_in_hand(state, card):
+        return (state, False, "You do not have this card")
+    else:
+        print("before", state.player_hand)
+        print("before", state.enemy_hand)
+        player_index = next((i for i, c in enumerate(state.player_hand) if c.value == card.value))
+        temp = state.player_hand[player_index]
+        state.player_hand[player_index] = state.enemy_hand[index]
+        state.enemy_hand[index] = temp
+
+        print(state.player_hand)
+        print(state.enemy_hand)
 
 
 def Change(state: State):
