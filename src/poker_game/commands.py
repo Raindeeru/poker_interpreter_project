186b423a11464861f9991ff35061ef9f6d6f4905
Gheyan <gclm4002@gmail.com>
2025-10-27 13:14:14 +0800

    fixing all in

--- src/poker_game/commands.py
+++ src/poker_game/commands.py
@@ -140,15 +140,22 @@ def Call(state: State):
 
 
 def All(state: State):
-    if state.enemy_last_bet == 0 or \
-            state.player_chips + state.player_last_bet == state.enemy_last_bet + state.enemy_chips:
+    if state.enemy_last_bet == 0:
         state.player_last_bet += state.player_chips
         state.player_chips = 0
-    else:
+
+    elif state.player_chips + state.player_last_bet < state.enemy_last_bet:
         state.player_last_bet += state.player_chips
+        state.player_chips = 0
         state.enemy_chips += state.enemy_last_bet - state.player_last_bet
         state.enemy_last_bet = state.player_last_bet
-        state.player_chips = 0
+
+    elif state.player_chips + state.player_last_bet > state.enemy_last_bet:
+        
+    else:
+        pass
+        
+
     state.player_all_in = True
     state.round_state = 2
     state.has_bet = True
